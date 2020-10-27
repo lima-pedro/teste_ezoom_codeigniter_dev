748d180851d66e54054b2cfb0dcb0eec2cb261e8
Pedro Lima <limma.pedro@gmail.com>
2020-10-27 16:25:06 -0300

    database configuration

--- application/views/pages/detalhe_curso.php
+++ application/views/pages/detalhe_curso.php
@@ -3,8 +3,10 @@
     <div class="card-header">
       <h3><?= $curso['titulo'] ?></h3>
     </div>
-    <div class="card-body">
-      <p><?= $curso['descricao'] ?></p>
+    <div class="conteudo-detalhe card-body">
+      <div class="descricao">
+        <p><?= $curso['descricao'] ?></p>
+      </div>
       <div class="galeria-imagens">
         <?php foreach ($img_galeria_nome as $img) { ?>
           <img src="<?= base_url()?>public/uploads/imgs_galeria/<?= $img ?>">
